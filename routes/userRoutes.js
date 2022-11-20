57c1351f1283fb73fc04e8f5b31756777be25ee2
Avishkar Vichare <avie9500@gmail.com>
2022-11-20 10:04:48 +0530

    added routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { route } = require('../app');
-const { getUsersController, createUserController, deleteUserController } = require('../controller/userController');
+const { getUsersController, createUserController, deleteUserController, editUserController } = require('../controller/userController');
 const router = express.Router();
 
 router.post('/createuser', createUserController);
-router.get('/getUser', getUsersController);
-router.delete('/delete', deleteUserController);
+router.get('/getusers', getUsersController);
+router.put('edituser/:id', editUserController);
+router.delete('/delete/:id', deleteUserController);
 
 module.exports = router;
